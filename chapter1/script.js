646ff19af5282a6374055d206790dc6f28dc013d
Ryunosuke Abe <ryu212920@gmail.com>
2020-10-13 14:47:42 +0900

    finish section2-1

--- chapter1/script.js
+++ chapter1/script.js
@@ -1,5 +1,25 @@
 let dom = document.querySelector('#root'); //querySelectorというメソッドを使ってid="root"のオブジェクトを取り出している。
 let element = React.createElement(
-  'p', {}, 'Hello React application!'
-);                                        //react特有の仮装DOMメソッドにてエレメントを作って組み込む
+  'div', {}, [
+    React.createElement(
+      'h2', {}, "Hello!"
+    ),
+    React.createElement(
+      'h3', {}, "React sample page."
+    ),
+    React.createElement(
+      'ul', {}, [
+        React.createElement(
+          'li', {}, "First item."
+        ),
+        React.createElement(
+          'li', {}, "Second item."
+        ),
+        React.createElement(
+          'li', {}, "third item."
+        ),
+      ]
+    ),
+  ]);
+                                       //react特有の仮装DOMメソッドにてエレメントを作って組み込む
 ReactDOM.render(element, dom);            //上記にて作成した仮装DOMエレメントをレンダリングにより表示する。
