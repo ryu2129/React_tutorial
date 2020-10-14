b797f34eec02d8636493d6ddd816c215692e1660
Ryunosuke Abe <ryu212920@gmail.com>
2020-10-14 17:31:38 +0900

    fix section3-2

--- react_app/src/index.js
+++ react_app/src/index.js
@@ -5,9 +5,7 @@ import App from './App';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <App title="App" message="This is App Component!" />,
   document.getElementById('root')
 );
 
