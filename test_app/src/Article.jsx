e069e417565634df65f5baf28375893ae419abe2
Ryunosuke Abe <ryu212920@gmail.com>
2020-10-25 22:13:06 +0900

    finish 5

--- test_app/src/Article.jsx
+++ test_app/src/Article.jsx
@@ -5,6 +5,7 @@
 // jsxをreturnする
 
 import React from 'react';
+import LikeButton from './LikeButton';
 
 const Article = (props) => {
   return (
@@ -12,6 +13,7 @@ const Article = (props) => {
       <h2>{props.title}</h2>
       <label htmlFor="check">公開状態:</label>
       <input type="checkbox" checked={props.isPublished} id="check" onClick={() => props.toggle()} />
+      <LikeButton count={props.count} />
     </div>
   )
 };
--- test_app/src/Blog.jsx
+++ test_app/src/Blog.jsx
@@ -10,23 +10,51 @@ class Blog extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      isPublished: false
+      isPublished: false,
+      count: 0
     }
+  }
+
+  // ボタンがクリックされたらいいねをカウントアップする
+  componentDidMount() {
+    document.getElementById('counter').addEventListener('click', this.countUp)
+  }
+
+  //　いいね数が10以上になったら0になる
+  componentDidUpdate() {
+    if (this.state.count >= 10 ) {
+      this.setState( {count:0})
+    }
+  }
+
+  componentWillUnmount() {
+    document.getElementById('counter').removeEventListener('click', this.countUp)
   }
 
   // 公開状況を反転させる関数
   // render内で値を変更してはいけない
-  // setState()で変更する（stateが変更されるとrenderが走るため無限ループしてしまう）
+  // setState()で変更する （stateが変更されるとrenderが走るため無限ループしてしまう）
   togglePublished = () => {
     this.setState({
       isPublished: !this.state.isPublished
     })
   };
 
+  countUp = () => {
+    this.setState({
+      count: this.state.count + 1
+    })
+  };
+
   render() {
     return (
       <>
-        <Article title={"Reactの使い方"} isPublished={this.state.isPublished} toggle={() => this.togglePublished()} />
+        <Article
+        title={"Reactの使い方"}
+        isPublished={this.state.isPublished}
+        toggle={() => this.togglePublished()}
+        count={this.state.count}
+        />
       </>
     )
   }
