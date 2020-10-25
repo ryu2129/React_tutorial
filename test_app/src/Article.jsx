7ad372e57cbfddd2303b90e60aac7ed874d0c52c
Ryunosuke Abe <ryu212920@gmail.com>
2020-10-25 20:32:18 +0900

    finish 4

--- test_app/src/Article.jsx
+++ test_app/src/Article.jsx
@@ -10,6 +10,8 @@ const Article = (props) => {
   return (
     <div>
       <h2>{props.title}</h2>
+      <label htmlFor="check">公開状態:</label>
+      <input type="checkbox" checked={props.isPublished} id="check" onClick={() => props.toggle()} />
     </div>
   )
 };
--- test_app/src/Blog.jsx
+++ test_app/src/Blog.jsx
@@ -9,15 +9,25 @@ import Article from './Article';
 class Blog extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {
+      isPublished: false
+    }
   }
+
+  // 公開状況を反転させる関数
+  // render内で値を変更してはいけない
+  // setState()で変更する（stateが変更されるとrenderが走るため無限ループしてしまう）
+  togglePublished = () => {
+    this.setState({
+      isPublished: !this.state.isPublished
+    })
+  };
+
   render() {
-    const authorName = "Yamada"
     return (
-      <div>
-        <Article title={"Reactの使い方"} />
-        <Article title={"Jsxの使い方"} />
-        <Article title={"環境構築をしてみよう"} />
-      </div>
+      <>
+        <Article title={"Reactの使い方"} isPublished={this.state.isPublished} toggle={() => this.togglePublished()} />
+      </>
     )
   }
 }
