e770a2bcdf777781607fe5bcc5a74602ba70b211
Ryunosuke Abe <ryu212920@gmail.com>
2020-10-26 00:58:51 +0900

    finis

--- test_app/src/Article.jsx
+++ test_app/src/Article.jsx
@@ -5,7 +5,7 @@
 // jsxをreturnする
 
 import React, {useState} from 'react';
-// import LikeButton from './LikeButton';
+import LikeButton from './LikeButton';
 
 const Article = (props) => {
   // isPublished→初期値がfalseのもの togglePublished→isPublishedを変更するものとして宣言
@@ -15,7 +15,7 @@ const Article = (props) => {
       <h2>{props.title}</h2>
       <label htmlFor="check">公開状態:</label>
       <input type="checkbox" checked={isPublished} id="check" onClick={() => togglePublished(!isPublished)} />
-      {/* <LikeButton count={props.count} /> */}
+      <LikeButton />
     </div>
   )
 };
